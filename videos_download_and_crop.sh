3a7afaaf025f11057fd1057d443924da6fda246d
yuangan <411246903@qq.com>
2023-10-18 15:47:22 +0800

    preprocess

--- videos_download_and_crop.sh
+++ videos_download_and_crop.sh
@@ -1,13 +1,13 @@
 dataset=$1
-s = 50 # start
-e = 100 # end
+s = 0 # start
+e = 5 # end
 # Download the videos.
 python videos_download.py --input_list data_list/${dataset}_video_ids.txt --output_dir ${dataset}/raw_videos --start  ${s} --end ${e}
 
 # Split the videos into 1-min chunks.
-#./videos_split.sh ${dataset}/raw_videos ${dataset}/1min_clips
+./videos_split.sh ${dataset}/raw_videos ${dataset}/1min_clips
 
 # Extract the talking head clips.
-#python videos_crop.py --input_dir ${dataset}/1min_clips/ --output_dir ${dataset}/cropped_clips --clip_info_file data_list/${dataset}_video_tubes.txt
+python videos_crop.py --input_dir ${dataset}/1min_clips/ --output_dir ${dataset}/cropped_clips --clip_info_file data_list/${dataset}_video_tubes.txt
 
-#python audios_crop.py --input_dir ${dataset}/1min_clips/ --output_dir ${dataset}/cropped_clips --clip_info_file data_list/${dataset}_video_tubes.txt
+python audios_crop.py --input_dir ${dataset}/1min_clips/ --output_dir ${dataset}/cropped_clips --clip_info_file data_list/${dataset}_video_tubes.txt
